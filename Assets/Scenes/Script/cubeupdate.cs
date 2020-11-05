7738ce75e35e71b59e5486acefdbaedcf2dc8063
minjoo1412 <50561483+minjoo1412@users.noreply.github.com>
2020-11-05 17:26:31 +0900

    게임소프트웨어 중간대체
    2020년 2학기 게임소프트웨어 중간대체과제
    1. Object 및 Asset 활용 + particle system 설정
    2. 자연환경 제작(Terrain)
    3. 백그라운드(quad) 움직임 구현

---
+++ Assets/Scenes/Script/cubeupdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;  
+
+public class cubeupdate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Cube update");
+    }
+}
